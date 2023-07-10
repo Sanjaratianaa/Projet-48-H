d0acdc80f6b89d2123ca3d489ae1b42eee149e78
olinaf11 <fanilokevin@icloud.com>
2023-07-10 11:39:47 +0300

    base

--- script/base.sql
+++ script/base.sql
@@ -3,15 +3,15 @@ create database regime;
 
 -- TABLES
     create table profil(
-        id_profil SERIAL PRIMARY KEY,
+        id SERIAL PRIMARY KEY,
         poids DOUBLE PRECISION,
         genre DOUBLE PRECISION,
         taille DOUBLE PRECISION,
-        date_report DATETIME
+        date_report date
     );
 
     create table utilisateur(
-        id_utilisateur SERIAL PRIMARY KEY,
+        id SERIAL PRIMARY KEY,
         nom VARCHAR(250),
         prenoms VARCHAR(250),
         mail VARCHAR(250),
@@ -24,6 +24,23 @@ create database regime;
         id_administrateur SERIAL PRIMARY KEY,
         mail VARCHAR(250),
         mot_de_passe VARCHAR(250)
+    );
+
+
+    create table categorie_aliment(
+        id SERIAL PRIMARY KEY,
+        designation VARCHAR(50)
+    );
+
+    create table aliment(
+        id SERIAL PRIMARY KEY,
+        id_categorie_aliment INT references categorie_aliment(id),
+        designation VARCHAR(50)
+    );
+
+    create table plat(
+        id serial primary key,
+        designation VARCHAR(50)
     );
 
 -- DATA
