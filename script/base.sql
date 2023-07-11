1cbdcf93ae08495a086c579d59f90f674cd8007c
Sanjaratianaa <sanjaratiana@yahoo.com>
2023-07-11 07:38:50 +0300

    table: code >> argent, demande, validation

--- script/base.sql
+++ script/base.sql
@@ -82,6 +82,29 @@ create database regime;
         foreign key (id_intensite) references intensite_physique(id)
     );
 
+    create table if not exists code_argent (
+        id VARCHAR(7) PRIMARY KEY,
+        montant DOUBLE PRECISION
+    );
+
+    create table if not exists code_demande (
+        id SERIAL PRIMARY KEY,
+        id_utilisateur INT,
+        id_code VARCHAR(7),
+        date_demande DATE,
+        foreign key (id_utilisateur) references utilisateur(id),
+        foreign key (id_code) references code_argent(id)
+    );
+
+    create table if not exists code_validation (
+        id SERIAL PRIMARY KEY,
+        id_code_demande INT,
+        id_administrateur INT,
+        date_validation DATE,
+        foreign key (id_code_demande) references code_demande(id), 
+        foreign key (id_administrateur) references administrateur(id) 
+    );
+
 -- ALTER
     -- UTILISATEUR
     ALTER TABLE utilisateur add column date_naissance DATE;
@@ -96,3 +119,21 @@ create database regime;
 
     create or replace view v_plat_regime as
         select pr.id, pr.id_plat, pr.id_regime, r.designation as designation_regime, p.designation as designation_plat from plat_regime pr join regime r on pr.id_regime = r.id join plat p on pr.id_plat = p.id;
+
+    create or replace view v_code_valide as
+        SELECT code_argent.* 
+            FROM code_argent 
+            LEFT JOIN 
+                (SELECT code_demande.id_code FROM code_validation
+                JOIN code_demande
+                ON code_validation.id_code_demande = code_demande.id) sous_requete 
+            ON code_argent.id = sous_requete.id_code WHERE sous_requete.id_code IS NULL;
+
+    create or replace view v_code_invalide as
+        SELECT code_argent.* 
+            FROM code_argent 
+            LEFT JOIN 
+                (SELECT code_demande.id_code FROM code_validation
+                JOIN code_demande
+                ON code_validation.id_code_demande = code_demande.id) sous_requete 
+            ON code_argent.id = sous_requete.id_code WHERE sous_requete.id_code IS NOT NULL;
